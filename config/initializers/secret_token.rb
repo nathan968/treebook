ae4f9c87f7c5f562d1d7a49d0b059940ca5e49c8
Nathan Anderson <nathan.anderson968@gmail.com>
2013-06-30 12:34:29 -0400

    Created a rials app and added some stats

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '23af604953eba792d54179c0d1ae5b2802f2cffa6750dd27b7222c345d7f1586dc6c60835bf2cbf7e9385c1955efe01d241e31d487703d1b6dc9131dc84ce02a'
